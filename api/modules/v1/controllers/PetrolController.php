4fcc4cd874bec4fe4498028ac7956e68fb879a62
vishnu patel <vishnu.patel@bacancytechnology.com>
2017-07-04 15:45:48 +0530

    changes for null response in city

--- api/modules/v1/controllers/PetrolController.php
+++ api/modules/v1/controllers/PetrolController.php
@@ -28,8 +28,8 @@ class PetrolController extends ActiveController {
         $modelClass = $this->modelClass;
         $model = $modelClass::find()->where($search)->all();
         $response = [];
-        
-        
+
+
         //petrol
         foreach ($model as $value) {
             $stateCode = $value->state_code;
@@ -38,7 +38,9 @@ class PetrolController extends ActiveController {
 
         if (!$this->search_array("iocl", $response) && !empty($response)) {
             $modelIocl = $modelClass::find()->where(['state_code' => $stateCode, 'type' => 'iocl'])->one();
-            $response['petrol'][] = ['state_code' => $modelIocl->state_code, 'city' => $modelIocl->city, 'price' => $modelIocl->price, 'change' => $modelIocl->change_diff, 'updated' => $value->updated, 'company' => 'iocl'];
+            if ($modelIocl !== null) {
+                $response['petrol'][] = ['state_code' => $modelIocl->state_code, 'city' => $modelIocl->city, 'price' => $modelIocl->price, 'change' => $modelIocl->change_diff, 'updated' => $value->updated, 'company' => 'iocl'];
+            }
         }
 
         //diesel
@@ -50,15 +52,17 @@ class PetrolController extends ActiveController {
 
         if (!$this->search_array("iocl", $response) && !empty($response)) {
             $modelIocl = \api\modules\v1\models\Diesel::find()->where(['state_code' => $stateCode, 'type' => 'iocl'])->one();
-            $response['diesel'][] = ['state_code' => $modelIocl->state_code, 'city' => $modelIocl->city, 'price' => $modelIocl->price, 'change' => $modelIocl->change_diff, 'updated' => $value->updated, 'company' => 'iocl'];
+            if ($modelIocl !== null) {
+                $response['diesel'][] = ['state_code' => $modelIocl->state_code, 'city' => $modelIocl->city, 'price' => $modelIocl->price, 'change' => $modelIocl->change_diff, 'updated' => $value->updated, 'company' => 'iocl'];
+            }
         }
 
         if (empty($response))
             return ['status' => 400, 'message' => 'Data not found.'];
 
-        return ['status'=>200,'message'=>'Data found.','data'=>$response];
+        return ['status' => 200, 'message' => 'Data found.', 'data' => $response];
     }
-    
+
     public function search_array($needle, $haystack) {
         if (in_array($needle, $haystack)) {
             return true;
